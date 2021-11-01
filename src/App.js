3981ccf4d8353493b9718e854779a5e0a2d435f2
krobingit <liorobin5@gmail.com>
2021-11-01 17:07:12 +0530

    React App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
+  const names = [{
+    name: "Robin",
+    dp: "https://images.unsplash.com/photo-1457449940276-e8deed18bfff?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZmlsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80"
+  }, {
+    name: "Rohit",
+      dp: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvxDrCR5SfO2zzeBNLF9U9xbjlC8-ToAA68g&usqp=CAU"
+    },
+    {
+      name: "John Doe",
+      dp: "http://www.goodmorningimagesdownload.com/wp-content/uploads/2019/10/Whatsapp-DP-2-2.jpg"
+    }]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {names.map((N) => <Msg name={N.name} pic={N.dp}/>)}
     </div>
   );
 }
-
-export default App;
+function Msg({ name, pic }) {
+  return (
+    <div>
+      <h1>Hello,{name}</h1>
+      <img class="picture" src={pic} alt={name}></img>
+    </div>
+  );
+}
