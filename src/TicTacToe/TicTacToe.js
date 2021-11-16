17d6be2fe3d7615874e063cd1f2154a21f21e0d4
krobingit <liorobin5@gmail.com>
2021-11-16 17:50:15 +0530

    Tic Tac Toe

--- src/TicTacToe/TicTacToe.js
+++ src/TicTacToe/TicTacToe.js
@@ -6,28 +6,28 @@ import Button from '@mui/material/Button';
 export function TicTacToe() {
 
 
- const [board, setBoard] = useState([null, null, null,
-  null, null, null, null, null, null]);
+  const [board, setBoard] = useState([null, null, null,
+    null, null, null, null, null, null]);
 
- const decideWinner = (board) => {
-  const lines = [
-   [0, 1, 2],
-   [3, 4, 5],
-   [6, 7, 8],
-   [0, 4, 8],
-   [2, 4, 6],
-   [0, 3, 6],
-   [1, 4, 7],
-   [2, 5, 8]
-  ];
-  for (let i = 0; i < lines.length; i++) {
-   const [a, b, c] = lines[i];
-   if (board[a] !== null && board[a] === board[b] && board[b] === board[c])
-    return board[a];
-  }
-  return null;
- };
- //Declaring winner variable
+  const decideWinner = (board) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (board[a] !== null && board[a] === board[b] && board[b] === board[c])
+        return board[a];
+    }
+    return null;
+  };
+  //Declaring winner variable
 
   var winner = decideWinner(board);
   /*
@@ -36,22 +36,22 @@ export function TicTacToe() {
   if (winner)
     setWinningText(winner+"WON!!! 🏆")
 */
- const [isXTurn, setIsXTurn] = useState(true);
+  const [isXTurn, setIsXTurn] = useState(true);
 
- const handleClick = (id) => {
-  if (winner === null && !board[id]) {
-   const boardCopy = [...board];
-   boardCopy[id] = isXTurn ? "X" : "O";
-   setBoard(boardCopy);
-   setIsXTurn(!isXTurn);
+  const handleClick = (id) => {
+    if (winner === null && !board[id]) {
+      const boardCopy = [...board];
+      boardCopy[id] = isXTurn ? "X" : "O";
+      setBoard(boardCopy);
+      setIsXTurn(!isXTurn);
+    }
+  };
+  function Box({ onPlayerClick, val }) {
+    //const [val, setVal] = useState(null);
+    return (<div className="gamebox"
+      onClick={onPlayerClick} style={{ color: val === "X" ? "green" : "red" }}>{val}</div>
+    );
   }
- };
- function Box({ onPlayerClick, val }) {
-  //const [val, setVal] = useState(null);
-  return (<div className="gamebox"
-   onClick={onPlayerClick} style={{ color: val === "X" ? "green" : "red" }}>{val}</div>
-  );
- }
 
   const { width, height } = useWindowSize();
 
@@ -61,19 +61,23 @@ export function TicTacToe() {
   }
 
 
- return (
-   <form className="full-game">
-   {winner ? <Confetti
-    width={width}
-    height={height} gravity={0.05}
-   /> : ""}
-   <div className="board">
-       {board.map((val, index) => <Box val={val} onPlayerClick={() => handleClick(index)} />)}
-       <Button variant="outlined" style={{ marginTop: "1rem" }} onClick={reset} >Restart</Button>
-   </div>
-     {winner ? <div className="winner"
-       style={{ color: winner === "X" ? "green" : "red" }}>🏆 Player {winner} WON!!!</div> : ""}
-  </form>
+  return (
+    <div className="full-game">
+      {winner ? <Confetti
+        width={width}
+        height={height} gravity={0.05}
+      /> : ""}
+      <div className="board">
+        {board.map((val, index) => <Box val={val} onPlayerClick={() => handleClick(index)} />)}
+      </div>
+      <div className="Reset">
+        <Button size="large" variant="contained" color="secondary" style={{ marginTop: "1rem",fontSize:"1.3rem" }} onClick={reset} >Restart</Button>
+      </div>
+      {winner ? <div className="winner"
+        style={{
+          color: winner === "X" ? "green" : "red", margin: "2rem", display: "flex", justifyContent:"center"
+        }}>🏆 Player {winner} WON!!!</div> : ""}
+    </div>
 
- );
+  );
 }
